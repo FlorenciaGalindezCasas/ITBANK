4075284dd8dc11120b90254f3cb5040e06d3c56e
Agustín Lorca <aguslorca.12@gmail.com>
2022-06-15 20:13:44 -0300

    dolar-ContadoLiq

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales%22")
+fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    miObj(data);
   });
+function miObj(data) {
+  let venta3 = document.getElementById("venta3");
+  venta3.innerHTML = `$${data[3].casa.venta}`;
+
+  let variacion3 = document.getElementById("variacion3");
+  variacion3.innerHTML = `Variación: ${data[3].casa.variacion}%`;
+}

522f480fb7ee5f968ff57fcd9695833a3bc912a9
FlorenciaDGC <florenciagalindezcasas@hotmail.com>
2022-06-15 19:16:01 -0300

    tarjeta dolar turista

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales%22")
+fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    miObj(data);
   });
+
+function miObj(data) {
+  let venta6 = document.getElementById("venta6");
+  venta6.innerHTML = `$${data[6].casa.venta}`;
+
+  let variacion6 = document.getElementById("variacion6");
+  variacion6.innerHTML = `Variacion: ${data[6].casa.variacion}%`;
+}

dfe2f7cc8bd4331bfe5d2fb07c182957728464fb
cvidal98 <canduvidal7@gmail.com>
2022-06-16 11:51:18 -0300

    dolar oficial y promedio

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
   });
 
 function miObj(data) {
-  let venta6 = document.getElementById("venta6");
-  venta6.innerHTML = `$${data[6].casa.venta}`;
+  let venta_promedio = document.getElementById("venta_promedio");
+  venta_promedio.innerHTML = `$${data[6].casa.venta}`;
 
-  let variacion6 = document.getElementById("variacion6");
-  variacion6.innerHTML = `Variacion: ${data[6].casa.variacion}%`;
+  let variacion_promedio = document.getElementById("variacion_promedio");
+  variacion_promedio.innerHTML = `Variacion: ${data[6].casa.variacion}%`;
 
-  let compra = document.getElementById("compra");
-  compra.innerHTML = `$${data[1].casa.compra}`;
+  let compra_promedio = document.getElementById("compra_promedio");
+  compra_promedio.innerHTML = `$${data[6].casa.compra}`;
 
-  let venta = document.getElementById("venta");
-  venta.innerHTML = `$${data[1].casa.venta}`;
+  let ventaOficial = document.getElementById("venta_precio");
+  venta_precio.innerHTML = `$${data[0].casa.venta}`;
 
-  let variacion = document.getElementById("variacion");
-  variacion.innerHTML = `Variacion: ${data[1].casa.variacion}%`;
+  let variacion_oficial = document.getElementById("variacion_precio");
+  variacion_precio.innerHTML = `Variacion: ${data[0].casa.variacion}%`;
+  let compra_oficial = document.getElementById("compra_precio");
+  compra_precio.innerHTML = `$${data[0].casa.compra}`;
 }
